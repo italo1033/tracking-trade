29e0e7d4dcc5848e100f059ad092d8a4ef5f15dc
italo1033 <italogammy1033@gmail.com>
2022-08-04 01:49:34 -0300

    add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,40 @@ export default function Home() {
   return(
     <div>
       <Navbar />
+      <div className="d-flex">
+        <section>
+          <div className={style.item}>
+
+          </div>
+          <div className={style.analise_percentual}>
+
+          </div>
+        </section>
+
+        {/* segundo container */}
+        <section className="w-100">
+          <header className={style.header_button}> 
+          ks
+          </header>
+          <main>
+            {/* charts  */}
+            <section className="d-flex">
+              <div className={style.charts}></div>
+              <div className={style.charts}></div>
+            </section>
+            {/* charts  */}
+            <section className="d-flex">
+              <div className={style.charts}></div>
+              <div className={style.charts}></div>
+            </section>
+            {/* charts  */}
+            <section className="d-flex">
+              <div className={style.charts}></div>
+              <div className={style.charts}></div>
+            </section>
+          </main>
+        </section>
+      </div>
     </div>
   );
 }
